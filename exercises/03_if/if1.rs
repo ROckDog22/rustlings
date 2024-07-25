1849fe6aa6b3f1bfa2aca8e3d34aa57fe1c2474b
rockdog22 <wangfengsheng1999@gamil.com>
2024-07-25 16:04:14 +0000

    string

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -30,3 +30,4 @@ mod tests {
         assert_eq!(42, bigger(42, 42));
     }
 }
+
--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -13,3 +13,15 @@ fn main() {
         println!("Good evening!");
     }
 }
+
+
+// 下面的代码没有任何错误
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+    
+    s.push_str("world");
+}
+
+fn borrow_object(s: &String) {
