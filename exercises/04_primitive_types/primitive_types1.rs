b98fa29c695a575dda53d23f02862f9a8cd8c114
Rockdog22 <wangfengsheng1999@gmail.com>
2024-07-26 08:15:09 +0000

    if and types

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -9,19 +9,8 @@ fn main() {
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
     // let …
+    let is_evening = true;
     if is_evening {
         println!("Good evening!");
     }
 }
-
-
-// 下面的代码没有任何错误
-fn main() {
-    let mut s = String::from("hello, ");
-
-    borrow_object(&s);
-    
-    s.push_str("world");
-}
-
-fn borrow_object(s: &String) {
